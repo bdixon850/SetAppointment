27992edb3b8bcc2fe5a5d1cdbd600c5230cfc7c5
Brandon Dixon <bdixon850@yahoo.com>
2019-06-07 05:34:44 -0400

    coding

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = require('./routes/index')
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', routes)
+
+app.use(express.static(__dirname + '/client/build/'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log("Magic happening on port " + PORT);
+});
